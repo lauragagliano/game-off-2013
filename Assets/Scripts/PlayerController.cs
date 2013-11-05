9aa0722ff8f87209cd8dfd2fd82cccd2bc3598a3
Edward Rowe <tehgiver@gmail.com>
2013-11-05 14:02:43 -0500

    Added very stub Player Controller. It moves an object up and down.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	
+	public float movespeed = 20.0f;
+	
+	void Update ()
+	{
+	
+		float vertical = Input.GetAxis ("Vertical");
+		if (Mathf.Abs (vertical) > 0) {
+			transform.Translate (new Vector3 (-(vertical * movespeed * Time.deltaTime), 0, 0));
+		}
+	}
+}

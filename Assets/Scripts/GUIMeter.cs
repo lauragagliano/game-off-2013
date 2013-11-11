ec21972cf961417c669bbf54f33ae6ea461a16fd
Edward Rowe <tehgiver@gmail.com>
2013-11-11 18:40:54 -0500

    Added Glow animations to each of the fill meters.`

--- Assets/Scripts/GUIMeter.cs
+++ Assets/Scripts/GUIMeter.cs
@@ -7,9 +7,11 @@ public class GUIMeter : MonoBehaviour
 	Player player;
 	float maxScale;
 	float maxSize;
-	
+	bool glowingUp = false;
 	public float CurrentFillPercent;
 	public GameObject fill;
+	public AnimationClip glowAnimation;
+	public Material fillMaterial;
 	
 	void Start ()
 	{
@@ -24,12 +26,22 @@ public class GUIMeter : MonoBehaviour
 	void Update ()
 	{
 		// Set scale to the current fill percentage
-		fill.transform.localScale = new Vector3(CurrentFillPercent * maxScale, fill.transform.localScale.y, 
+		fill.transform.localScale = new Vector3 (CurrentFillPercent * maxScale, fill.transform.localScale.y, 
 			fill.transform.localScale.z);
 		
 		// Adjust "anchoring" of the fill to always be to the left of the parent object
-		float missingX = maxSize * (1-CurrentFillPercent) * maxScale;
-		fill.transform.localPosition = new Vector3(-missingX, fill.transform.localPosition.y,
+		float missingX = maxSize * (1 - CurrentFillPercent) * maxScale;
+		fill.transform.localPosition = new Vector3 (-missingX, fill.transform.localPosition.y,
 			fill.transform.localPosition.z);
+		
+		// Glow the full meter
+		if (CurrentFillPercent >= 1.0f) {
+			Animation animation = GetComponent<Animation>();
+			animation.Play (glowAnimation.name);
+		} else {
+			Animation animation = GetComponent<Animation>();
+			animation.Stop();
+			fill.renderer.material = fillMaterial;
+		}
 	}
 }

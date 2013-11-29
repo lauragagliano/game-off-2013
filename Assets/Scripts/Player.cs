f12d87f4d2baa29ed57370b24fbc0f08a2c70d8a
Edward Rowe <tehgiver@gmail.com>
2013-11-29 13:39:05 -0500

    Undid debug code to always give boost. Re-linked missing prefabs on GameManager.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -794,8 +794,6 @@ public class Player : MonoBehaviour
 	{
 		pickups = new List<GameObject> ();
 		
-		
-		inventory.AddItem(ItemNames.BOOST);
 		// Give player their upgrades
 		// Charges Faster Upgrades
 		if (inventory.HasItem (ItemNames.BLUE_FILLS_FASTER)) {
